1e3a9901bc9cf3f6ce414cb6dad27f6f7a64ddaf
louisvanlinden <louis.vanlinden@ugent.be>
2025-10-31 16:27:27 +0100

    einde van de dag :)) het werkt normaal

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
-# config.py
 import os
 
 class Config:
-    # Dit haalt de database-URL uit je .env-bestand
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
-
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_pre_ping": True,
-    }
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
