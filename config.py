7e2772af8aad69c7008a3d50d4755a5f757cf6a6
saparjen <sap.arjen@gmail.com>
2025-11-03 15:33:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SECRET_KEY = 'your_secret_key'  # mag eender wat zijn
+    SECRET_KEY = 'Team_38_Maspoe'  
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Team_38_Maspoe@db.pwvyxvgwntypsbyuhkwa.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
