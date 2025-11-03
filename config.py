efe43f97d53d003d038202365628ad470bc0e4b8
KorneelPennewaert <korneel.pennewaert@ugent.be>
2025-11-03 16:18:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,3 @@ class Config:
     SECRET_KEY = 'Team_38_Maspoe'  
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Team_38_Maspoe@db.pwvyxvgwntypsbyuhkwa.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
