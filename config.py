b5f20423a2c108acec8f150da78e756650f6c71a
saparjen <sap.arjen@gmail.com>
2025-11-03 15:32:42 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-import os
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = 'your_secret_key'  # mag eender wat zijn
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Team_38_Maspoe@db.pwvyxvgwntypsbyuhkwa.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
-    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
