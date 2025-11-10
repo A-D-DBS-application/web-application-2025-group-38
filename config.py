95922ad28142046469aa390dcb161af2aff654bb
KorneelPennewaert <korneel.pennewaert@ugent.be>
2025-11-10 17:35:40 +0100

    update log in + weer verbonden met supa

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
     SQLALCHEMY_DATABASE_URI = (
-        "postgresql://postgres:Team_38_Maspoe@db.pwvyxvgwntypsbyuhkwa.supabase.co:5432/postgres?sslmode=require"
+        "postgresql+psycopg2://postgres.pwvyxvgwntypsbyuhkwa:Team_38_Maspoe@aws-1-eu-north-1.pooler.supabase.com:6543/postgres"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
