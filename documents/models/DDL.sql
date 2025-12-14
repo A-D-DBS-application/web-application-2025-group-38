d6e7dac3eb30731a9f104c64d55c5fa1811a2171
judceu <judith.ceuppens@ugent.be>
2025-12-14 11:09:13 +0100

    Update and rename DDL to DDL.sql

--- documents/models/DDL.sql
+++ documents/models/DDL.sql
@@ -1,5 +1,4 @@
--- WARNING: This schema is for context only and is not meant to be run.
--- Table order and constraints may not be valid for execution.
+
 
 CREATE TABLE public.ArtistEdition (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
